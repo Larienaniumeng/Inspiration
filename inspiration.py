a0e6041083884d3e28fe83db8079f1eb161b270d
lw2781 <lw2781@columbia.edu>
2018-04-24 21:07:48 +0000

    Fix bug

--- inspiration.py
+++ inspiration.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def random_quotes():
     with open('inspiration.txt', 'r', encoding='utf-8') as fp:
-	lines = fp.read().split('\n')
+        lines = fp.read().split('\n')
     quote = random.choice(lines)
-   return quote
+    return quote
