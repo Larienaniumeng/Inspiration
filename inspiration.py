5a26802e3be3070fd83a0d05abf1fc479d9f4a0e
lw2781 <lw2781@columbia.edu>
2018-04-24 21:02:29 +0000

    Added a python file

---
+++ inspiration.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def random_quotes():
+    with open('inspiration.txt', 'r', encoding='utf-8') as fp:
+	lines = fp.read().split('\n')
+    quote = random.choice(lines)
+   return quote
